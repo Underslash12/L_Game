92a2a82c2110633a47d81a25ef230874795d0918
Underslash12 <stew_smith@hotmail.com>
2019-11-09 19:09:31 -0800

    Added flip + rotation and block control

--- GridPanel.java
+++ GridPanel.java
@@ -1,6 +1,7 @@
 // TestPanel.java
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
@@ -8,18 +9,23 @@ import java.awt.geom.Rectangle2D.Double;
 import java.awt.Color;
 import java.awt.event.*;
 
-public class GridPanel extends JPanel implements MouseMotionListener, MouseListener{
+public class GridPanel extends JPanel implements MouseMotionListener, MouseListener, MouseWheelListener{
 	
 	boolean draw = false;
 	// Rectangle2D[] orangeLMain = new Rectangle2D[4];
 	// Rectangle2D[] orangeLHelper;
-	LPiece orange;
+	LPiece orange, orange2, orangeShadow;
 	boolean moveable;
+	
+	int scrollSensitivity;
+	double scrollCounter;
 	
 	double xOffset;
 	double yOffset;
 	double xScale;
 	double yScale;
+	
+	double shadow = 7;
 	
 	public GridPanel() 
 	{
@@ -28,15 +34,21 @@ public class GridPanel extends JPanel implements MouseMotionListener, MouseListe
 		setBackground(new Color(bg, bg, bg));
 		addMouseMotionListener(this);
 		addMouseListener(this);
+		addMouseWheelListener(this);
 		
 		xOffset = 0;
 		yOffset = 0;
 		xScale = 1;
 		yScale = 1;
 		
+		scrollSensitivity = 2;
+		scrollCounter = 0;
+		
 		// orangeLMain[0] = new Rectangle2D.Double(155 + 36, 155 + 36, 109 * xScale, 109 * yScale);
 		// orange = new LPiece(155 + 36, 155 + 36, 1, new Color(0, 0, 200));
-		orange = new LPiece(155 + 26, 155 + 26, 1, new Color(0, 0, 200));
+		orange = new LPiece(181, 181, 1, new Color(0, 0, 200));
+		orange2 = new LPiece(181 + shadow, 181 - shadow, 1, new Color(0, 0, 150)); //hmmm
+		orangeShadow = new LPiece(181, 181, 1, new Color(170, 170, 255));
 	}
 	
 	@Override
@@ -65,9 +77,12 @@ public class GridPanel extends JPanel implements MouseMotionListener, MouseListe
 				}
 			}
 		}
-		
-		// g2d.setColor(new Color(0, 0, 200));
+		if (moveable) {
+			orangeShadow.paintComponent(g2d);
+			orange2.paintComponent(g2d);
+		}
 		orange.paintComponent(g2d);
+		
     }
 	
 	// Used mouse events
@@ -77,28 +92,68 @@ public class GridPanel extends JPanel implements MouseMotionListener, MouseListe
 		if (moveable) {
 			// System.out.println("test2");
 			// orange = new Rectangle2D.Double(e.getX() - 55, e.getY() - 55, 109, 109);
-			orange.translateTo(e.getX(), e.getY());
-			repaint();
+			orange.translateTo(e.getX() - orange.getCenterX(), e.getY() - orange.getCenterY());
+			orange2.translateTo(orange.getX() + shadow, orange.getY() - shadow);
+			orangeShadow.translateTo(
+				(36 + xOffset) + (int)((orange.getX() + 20) / 145) * 145, 
+				(36 + yOffset) + (int)((orange.getY() + 20) / 145) * 145
+			);
 		}
+		repaint();
     }
 	
 	public void mousePressed(MouseEvent e) 
 	{
-		// System.out.println("test3");
-		if (orange.contains(e.getX(), e.getY())) {
-			// System.out.println("test4");
-			moveable = !moveable;
-			mouseMoved(e);
+		if (SwingUtilities.isLeftMouseButton(e)) {
+			if (orange.contains(e.getX(), e.getY())) {
+				// System.out.println("test4");
+				moveable = !moveable;
+				if (!moveable) {
+					orange.translateTo(orangeShadow.getX(), orangeShadow.getY());
+				}
+				mouseMoved(e);
+			}
 		}
+		else if (SwingUtilities.isRightMouseButton(e)) {
+			if (moveable) {
+				orange.flip();
+				orange2.flip();
+				orangeShadow.flip();
+				
+				repaint();
+			}
+		}
+		// SwingUtilities.isMiddleMouseButton(e);
+		
+		// System.out.println("test3");
+		
 	}
 	
-	public void mouseReleased(MouseEvent e) 
-	{
-		// moveable = false;
+	public void mouseWheelMoved(MouseWheelEvent e)
+	{	
+		// System.out.println(e.getScrollType());
+		
+		scrollCounter += Math.abs(e.getPreciseWheelRotation());
+		if (scrollCounter >= scrollSensitivity) {
+			if (moveable) {
+				int amountToRotate = e.getPreciseWheelRotation() > 0 ? 1 : 3;
+				
+				orange.rotate90(amountToRotate);
+				orange2.rotate90(amountToRotate);
+				orangeShadow.rotate90(amountToRotate);
+				
+				repaint();
+			}
+			
+			scrollCounter = 0;
+		}
+		
+		
 	}
 	
 	
 	// Unusued mouse events
+	public void mouseReleased(MouseEvent e) {}
 	public void mouseClicked(MouseEvent e) {}
 	public void mouseEntered(MouseEvent e) {}
 	public void mouseExited(MouseEvent e) {}
--- LPiece.java
+++ LPiece.java
@@ -11,6 +11,7 @@ public class LPiece {
 
 	private Rectangle2D[] rects = new Rectangle2D[2];
 	private Color color;
+	private double scale;
 
 	public LPiece (double x, double y, double scale, Color c)
 	{
@@ -18,26 +19,84 @@ public class LPiece {
 		rects[0] = new Rectangle2D.Double(x, y, 129 * scale, 419 * scale);
 		rects[1] = new Rectangle2D.Double(x + 129, y + 419 - 129, (129 + 16) * scale, 129 * scale);
 		color = c;
+		this.scale = scale;
 	}
 	
-	public double getX() 
+	public double getX () 
 	{
 		return rects[0].getX();
 	}
 	
-	public double getY() 
+	public double getY () 
 	{
 		return rects[0].getY();
 	}
 	
-	public void rotate90 (int times)
+	public double getCenterX ()
 	{
-		
+		return rects[0].getWidth() / 2;
 	}
 	
-	public void flipHorizontally ()
+	public double getCenterY ()
 	{
-		
+		return rects[0].getHeight() / 2;
+	}
+	
+	public void rotate90 (int times)
+	{
+		for (int i = 0; i < times; i++) {
+			for (int j = 0; j < rects.length; j++) {
+				double  x1 = rects[j].getX(), 
+						y1 = rects[j].getY(), 
+						x2 = rects[j].getX() + rects[j].getWidth(),
+						y2 = rects[j].getY() + rects[j].getHeight();
+				
+				double[] p1 = rotate90Point(x1, y1, getCenterX() + getX(), getCenterY() + getY());
+				double[] p2 = rotate90Point(x2, y2, getCenterX() + getX(), getCenterY() + getY());
+				
+				rects[j] = createRectangleFromPoints(
+					p1[0],
+					p1[1],
+					p2[0],
+					p2[1]
+				);
+			}
+		}
+	}
+	
+	// rotate 90 degrees formula is (x, y) -> (-(y - b) + a, (x - a) + b)
+	private double[] rotate90Point (double x, double y, double a, double b)
+	{
+		return new double[]{-(y - b) + a, (x - a) + b};
+	}
+	
+	public void flip ()
+	{
+		for (int i = 0; i < rects.length; i++) {
+			rects[i] = createRectangleFromPoints(
+				-(rects[i].getX() - (getCenterX() + getX())) + (getCenterX() + getX()),
+				rects[i].getY(),
+				-(rects[i].getX() + rects[i].getWidth() - (getCenterX() + getX())) + (getCenterX() + getX()),
+				rects[i].getY() + rects[i].getHeight()
+			);
+		}
+		// double x = rects[0].getX();
+		// double x2 = rects[1].getX();
+		// double w = rects[0].getWidth();
+		// double w2 = rects[1].getWidth();
+		// double newX = 0;
+		// newX = x > x2 ? w + w2 : -w - w2;
+		// rects[1] = new Rectangle2D.Double(rects[1].getX() + newX, rects[1].getY(), rects[1].getWidth(), rects[1].getHeight());
+	}
+	
+	private Rectangle2D createRectangleFromPoints(double x1, double y1, double x2, double y2)
+	{
+		return new Rectangle2D.Double(
+			Math.min(x1, x2),
+			Math.min(y1, y2),
+			Math.abs(x1 - x2),
+			Math.abs(y1 - y2)
+		);
 	}
 	
 	public boolean contains (double x, double y)
